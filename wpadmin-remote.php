1318596e6ed6035e5edede8cc415c7595c214fd0
abhijit <abhijit.rakas@rtcamp.com>
2018-09-25 23:57:26 +0530

    Add check for woocommerce urls

--- wpadmin-remote.php
+++ wpadmin-remote.php
@@ -39,34 +39,6 @@ function wpadminremote_load_scripts() {
 		'pgn'    => admin_url( 'post-new.php?post_type=page' ),
 		'pgs'    => admin_url( 'edit.php?s=%s%&post_type=page' ),
 		'cm'     => admin_url( 'edit-comments.php' ),
-		'wc'     => admin_url( 'edit.php?post_type=shop_order' ),
-		'wcos'   => admin_url( 'edit.php?s=%s%&post_type=shop_order' ),
-		'wcn'    => admin_url( 'post-new.php?post_type=shop_order' ),
-		'wcc'    => admin_url( 'edit.php?post_type=shop_coupon' ),
-		'wccn'   => admin_url( 'post-new.php?post_type=shop_coupon' ),
-		'wcro'   => admin_url( 'admin.php?page=wc-reports' ),
-		'wcrc'   => admin_url( 'admin.php?page=wc-reports&tab=customers' ),
-		'wcrs'   => admin_url( 'admin.php?page=wc-reports&tab=stock' ),
-		'wcrt'   => admin_url( 'admin.php?page=wc-reports&tab=taxes' ),
-		'wcs'    => admin_url( 'admin.php?page=wc-settings' ),
-		'wcsp'   => admin_url( 'admin.php?page=wc-settings&tab=products' ),
-		'wcstx'  => admin_url( 'admin.php?page=wc-settings&tab=tax' ),
-		'wcss'   => admin_url( 'admin.php?page=wc-settings&tab=shipping' ),
-		'wcspyt' => admin_url( 'admin.php?page=wc-settings&tab=checkout' ),
-		'wcsap'  => admin_url( 'admin.php?page=wc-settings&tab=account' ),
-		'wcse'   => admin_url( 'admin.php?page=wc-settings&tab=email' ),
-		'wcsa'   => admin_url( 'admin.php?page=wc-settings&tab=advanced' ),
-		'wcst'   => admin_url( 'admin.php?page=wc-status' ),
-		'wcstt'  => admin_url( 'admin.php?page=wc-status&tab=tools' ),
-		'wcstl'  => admin_url( 'admin.php?page=wc-status&tab=logs' ),
-		'wce'    => admin_url( 'admin.php?page=wc-addons' ),
-		'wces'   => admin_url( 'admin.php?page=wc-addons&section=helper' ),
-		'wcp'    => admin_url( 'edit.php?post_type=product' ),
-		'wcps'   => admin_url( 'edit.php?s=%s%&post_type=product' ),
-		'wcpn'   => admin_url( 'post-new.php?post_type=product' ),
-		'wcpc'   => admin_url( 'edit-tags.php?taxonomy=product_cat&post_type=product' ),
-		'wcpt'   => admin_url( 'edit-tags.php?taxonomy=product_tag&post_type=product' ),
-		'wcpa'   => admin_url( 'edit.php?post_type=product&page=product_attributes' ),
 		'at'     => admin_url( 'themes.php' ),
 		'ac'     => admin_url( 'customize.php?return=%2Fwp-admin%2Fthemes.php' ),
 		'aw'     => admin_url( 'widgets.php' ),
@@ -92,6 +64,45 @@ function wpadminremote_load_scripts() {
 		'sp'     => admin_url( 'options-permalink.php' ),
 		'spr'    => admin_url( 'privacy.php' ),
 	];
+
+	include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+	if ( is_plugin_active( 'woocommerce/woocommerce.php' ) ) {
+
+		$woo_urls = [
+			'wc'     => admin_url( 'edit.php?post_type=shop_order' ),
+			'wcos'   => admin_url( 'edit.php?s=%s%&post_type=shop_order' ),
+			'wcn'    => admin_url( 'post-new.php?post_type=shop_order' ),
+			'wcc'    => admin_url( 'edit.php?post_type=shop_coupon' ),
+			'wccn'   => admin_url( 'post-new.php?post_type=shop_coupon' ),
+			'wcro'   => admin_url( 'admin.php?page=wc-reports' ),
+			'wcrc'   => admin_url( 'admin.php?page=wc-reports&tab=customers' ),
+			'wcrs'   => admin_url( 'admin.php?page=wc-reports&tab=stock' ),
+			'wcrt'   => admin_url( 'admin.php?page=wc-reports&tab=taxes' ),
+			'wcs'    => admin_url( 'admin.php?page=wc-settings' ),
+			'wcsp'   => admin_url( 'admin.php?page=wc-settings&tab=products' ),
+			'wcstx'  => admin_url( 'admin.php?page=wc-settings&tab=tax' ),
+			'wcss'   => admin_url( 'admin.php?page=wc-settings&tab=shipping' ),
+			'wcspyt' => admin_url( 'admin.php?page=wc-settings&tab=checkout' ),
+			'wcsap'  => admin_url( 'admin.php?page=wc-settings&tab=account' ),
+			'wcse'   => admin_url( 'admin.php?page=wc-settings&tab=email' ),
+			'wcsa'   => admin_url( 'admin.php?page=wc-settings&tab=advanced' ),
+			'wcst'   => admin_url( 'admin.php?page=wc-status' ),
+			'wcstt'  => admin_url( 'admin.php?page=wc-status&tab=tools' ),
+			'wcstl'  => admin_url( 'admin.php?page=wc-status&tab=logs' ),
+			'wce'    => admin_url( 'admin.php?page=wc-addons' ),
+			'wces'   => admin_url( 'admin.php?page=wc-addons&section=helper' ),
+			'wcp'    => admin_url( 'edit.php?post_type=product' ),
+			'wcps'   => admin_url( 'edit.php?s=%s%&post_type=product' ),
+			'wcpn'   => admin_url( 'post-new.php?post_type=product' ),
+			'wcpc'   => admin_url( 'edit-tags.php?taxonomy=product_cat&post_type=product' ),
+			'wcpt'   => admin_url( 'edit-tags.php?taxonomy=product_tag&post_type=product' ),
+			'wcpa'   => admin_url( 'edit.php?post_type=product&page=product_attributes' ),
+		];
+
+		$urls = array_merge( $urls, $woo_urls );
+
+	}
 
 	wp_localize_script(
 		'wpadminremote-admin-js',
